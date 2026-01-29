39a8eb91e07740c407a131612964e9d97a87f120
cityo1 <ansrnrgus1@naver.com>
2026-01-29 16:31:27 +0900

    sidebar redux

--- fullstack_dashboard/front/src/App.jsx
+++ fullstack_dashboard/front/src/App.jsx
@@ -1,15 +1,14 @@
-import Appbar from './components/baseLayout/Appbar'
-import Sidebar from './components/baseLayout/Sidebar'
-import DashboardScreen from './components/dashboard/DashboardScreen'
-import './index.css'
+import Appbar from './components/baseLayout/Appbar';
+import Sidebar from './components/baseLayout/Sidebar';
+import DashboardScreen from './components/dashboard/DashboardScreen';
+import './index.css';
 
 function App() {
-
   return (
-    <div className='App w-screen flex items-center justify-center flex-col dark:bg-[#212121] dark:text-white'>
-      <div className='page-wrapper min-h-screen flex w-full'>
+    <div className="App w-screen flex items-center justify-center flex-col dark:bg-[#212121] dark:text-white">
+      <div className="page-wrapper min-h-screen flex w-full">
         <Sidebar />
-        <div className='content-wrapper w-full'> 
+        <div className="content-wrapper w-full ">
           <Appbar />
           <DashboardScreen />
         </div>
--- fullstack_dashboard/front/src/components/dashboard/DashboardScreen.jsx
+++ fullstack_dashboard/front/src/components/dashboard/DashboardScreen.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import Sales from './Sales'
-import Visitors from './Visitors'
-import TotalRevenue from './TotalRevenue'
-import Customers from './Customers'
-import TargetReality from './TargetReality'
-import TopProduct from './TopProduct'
-import SalesMap from './SalesMap'
-import VolumeServices from './VolumeServices'
+import React from 'react';
+import Sales from './Sales';
+import Visitors from './Visitors';
+import TotalRevenue from './TotalRevenue';
+import Customers from './Customers';
+import TargetReality from './TargetReality';
+import TopProduct from './TopProduct';
+import SalesMap from './SalesMap';
+import VolumeServices from './VolumeServices';
 
 const DashboardScreen = () => {
   return (
-    <div className='w-[calc(80%-28px)] ml-[calc(20%+14px)] mt-[14px]'>
-      <div className='grid grid-cols-[4fr_3fr] gap-x-[14px]'>
+    <div className="w-[calc(80%-28px)] ml-[calc(20%+14px)] mt-[14px]">
+      <div className="grid grid-cols-[4fr_3fr] gap-x-[14px]">
         <Sales />
         <Visitors />
       </div>
-      <div className='grid grid-cols-[3fr_2fr_2fr] gap-x-[14px]'>
+      <div className="grid grid-cols-[3fr_2fr_2fr] gap-x-[14px]">
         <TotalRevenue />
         <Customers />
         <TargetReality />
       </div>
-      <div className='grid grid-cols-[3fr_2fr_2fr] gap-x-[14px]'>
+      <div className="grid grid-cols-[3fr_2fr_2fr] gap-x-[14px]">
         <TopProduct />
         <SalesMap />
         <VolumeServices />
       </div>
     </div>
-  )
-}
-
-export default DashboardScreen
+  );
+};
+export default DashboardScreen;
